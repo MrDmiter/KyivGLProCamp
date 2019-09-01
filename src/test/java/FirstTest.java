c65aa1a44f77282df2d75f471569c45e11c5d883
MrDmiter <ter.dmi.ol@gmail.com>
2019-09-01 15:55:38 +0300

    removed extra folders and files, updated test case after site updates

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -43,14 +43,14 @@ public class FirstTest {
         //Wait until login button is clickable
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button"))).click();
         //Wait until the slowest element on th page is uploaded
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//img[@alt='English']")));
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//div[@id='copyright']"),"Copyright © 2012-2019"));
         //Find all the visible categories from left bar
-        List<WebElement> categories = driver.findElements(By.xpath("//ul[@class='list-vertical']/li"));
+        List<WebElement> categories = driver.findElements(By.xpath("//ul[@id='box-apps-menu']/li"));
         //Take each element and click on it
         for (int j = 0; j < categories.size(); j++) {
             categories.get(j).click();
             //Find h1 element on the page
-            WebElement h1 = driver.findElement(By.xpath("//h1"));
+            WebElement h1 = driver.findElement(By.xpath("//div[@class='panel-heading']"));
             //Ensure that title is present and displayed on the page
             Assert.assertTrue(h1.isDisplayed());
             //Find all visible subcategories
@@ -59,12 +59,12 @@ public class FirstTest {
             if (subCategories.size() != 0) {
                 for (int i = 0; i < subCategories.size(); i++) {
                     subCategories.get(i).click();
-                    h1 = driver.findElement(By.xpath("//h1"));
+                    h1 = driver.findElement(By.xpath("//div[@class='panel-heading']"));
                     Assert.assertTrue(h1.isDisplayed());
                     subCategories = driver.findElements(By.xpath("//ul[@class='docs']/li"));
                 }
             }
-            categories = driver.findElements(By.xpath("//ul[@class='list-vertical']/li"));
+            categories = driver.findElements(By.xpath("//ul[@id='box-apps-menu']/li"));
         }
     }
 
